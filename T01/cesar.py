723eace382880e9387ea3fc732bcc7a47dbab95b
Angelin01 <angeloborsoiross@hotmail.com>
2018-08-15 18:27:02 -0300

    Cifra de cesar pronta

--- T01/cesar.py
+++ T01/cesar.py
@@ -1,5 +1,5 @@
 import sys
-
+import argparse
 
 def cypher(message, key, chars):
 	cyphered = ""
@@ -22,7 +22,24 @@ def decypher(message, key, chars):
 	return decyphered
     
 def main():
+	# List of characters to consider on the cypher, all others are ignored
 	characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	
+	# Parsing arguments
+	parser = argparse.ArgumentParser(description='Cifrar ou decifrar mensagens com cifra de cesar.')
+	function = parser.add_mutually_exclusive_group(required=True)
+	function.add_argument('-c', '--cypher', action='store_true', help="Cifrar a mensagem")
+	function.add_argument('-d', '--decypher', action='store_true', help="Decifrar a mensagem")
+	parser.add_argument('-k', '--key', action='store', type=int, required=True, help="A chave da criptografia")
+	parser.add_argument('file', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
+	args = parser.parse_args()
+	
+	args.key = args.key % len(characters)
+	
+	if args.cypher:
+		print(cypher(args.file.read(), args.key, characters))
+	elif args.decypher:
+		print(decypher(args.file.read(), args.key, characters))
 	
 if __name__ == "__main__":
 	main()
