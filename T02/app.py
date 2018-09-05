2f461c30e74d3874a8b943b59e66fa7683aa1cfb
Angelin01 <angeloborsoiross@hotmail.com>
2018-09-05 18:08:59 -0300

    Trab 2 pronto

---
+++ T02/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+from hashlib import sha256
+from time import time
+
+
+def main():
+	while True:
+		username = input("\nDigite seu usuário: ")
+		seed = None
+
+		with open('users', 'a+'):
+			pass
+		with open('users') as users:
+			for user in users.readlines():
+				user = user.strip('\n').split(',')
+				if user[0] == username:
+					seed = user[2]
+
+		if not seed:
+			print("Usuário não existe!")
+			continue
+		print("Os tokens para este minuto são:")
+		minSeedPass = seed + str(int(time()) - int(time()) % 60)
+		tokens = [sha256(minSeedPass.encode('utf-8')).hexdigest()]
+		print(tokens[0][10:16].upper())
+		for i in range(1,5):
+			tokens.append(sha256(tokens[i-1].encode('utf-8')).hexdigest())
+			print(tokens[i][10:16].upper())
+
+if __name__ == "__main__":
+	main()
--- T02/servidor.py
+++ T02/servidor.py
@@ -2,26 +2,42 @@
 from hashlib import sha256
 from getpass import getpass
 from time import time
+import string
+import random
+import os
 
 def registerNewUser():
+	# Gerar sal aleatorio de tamanho 4
+	salt = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for _ in range(4)).encode('utf-8')
+
 	username = input("Digite o username do novo usuário: ").strip('\n')
-	password = sha256(getpass("Digite a senha do novo usuário: ").strip('\n').encode('utf-8')).hexdigest()
-	passwordConfirm = sha256(getpass("Digite novamente a mesma senha: ").strip('\n').encode('utf-8')).hexdigest()
+	password = sha256(getpass("Digite a senha do novo usuário: ").strip('\n').encode('utf-8') + salt).hexdigest()
+	passwordConfirm = sha256(getpass("Digite novamente a mesma senha: ").strip('\n').encode('utf-8') + salt).hexdigest()
 
 	if password != passwordConfirm:
 		print("*** Senhas não batem, tente novamente ***\n")
 		return
 		
-	seedPassword = sha256((username+password).encode('utf-8')).hexdigest()
-		
+	seedPassword = sha256(getpass("Digite a senha semente: ").strip('\n').encode('utf-8') + salt).hexdigest()
+	seedPasswordConfirm = sha256(getpass("Digite novamente a senha semente: ").strip('\n').encode('utf-8') + salt).hexdigest()
+
+	if seedPassword != seedPasswordConfirm:
+		print("*** Senhas semente não batem, tente novamente ***\n")
+		return
+
 	with open('users', 'a') as users:
-		users.write('{},{},{}\n'.format(username, password, seedPassword))
+		users.write('{},{},{},{}\n'.format(username, password, seedPassword, salt.decode('utf-8')))
 		
 	print("")
 	
 def authenticate():
+	# Autenticação normal
 	username = input("Digite seu username: ").strip('\n')
 	authInfo = None
+
+	with open('users', 'a+'):   # Criar arquivo de autenticacao se nao existe
+		pass
+
 	users = open('users', 'r').readlines()
 	for user in users:
 		user = user.strip('\n').split(',')
@@ -31,19 +47,49 @@ def authenticate():
 		print("Usuário inválido!\n")
 		return
 	
-	password = sha256(getpass("Digite sua senha: ").strip('\n').encode('utf-8')).hexdigest()
+	password = sha256(getpass("Digite sua senha: ").strip('\n').encode('utf-8') + authInfo[3].encode('utf-8')).hexdigest()
 	if password != authInfo[1]:
 		print("Senha incorreta!\n")
 		return
+
+	# Ler tokens invalidos para esse usuario
+	with open('invalidTokens', 'a+'):  # Criar arquivo de invalidos se nao existe
+		pass
+
+	invalidatedTokens = []
+	with open('invalidTokens', 'r') as invalidFile:
+		for line in invalidFile.readlines():
+			line = line.strip('\n').split(',')
+			if line[0] == authInfo[0]:
+				invalidatedTokens.append(line[1])
 	
 	token = input("Digite seu token: ").strip('\n').upper()
+	# Checa o tempo só depois que o usuario digita o token por facilidade
 	minSeedPass = authInfo[2] + str(int(time()) - int(time())%60)
-	possiblePasswords = [sha256(minSeedPass.encode('utf-8')).hexdigest()]
+
+	# Gera o primeiro token
+	possibleTokens = [sha256(minSeedPass.encode('utf-8')).hexdigest()]
+	if possibleTokens[0] in invalidatedTokens:  # Checa se o token não é invalido
+		print("Token inválido!\n")
+		return
+	if token == possibleTokens[0][10:16].upper():  # Checa se o token é o digitado
+		print("Autenticação realizada com sucesso!\n")
+		with open('invalidTokens', 'a') as invalidTokens:
+			invalidTokens.write("{},{}\n".format(authInfo[0], possibleTokens[0]))
+		return
+
+	# Gera os demais tokens, 1 de cada vez, verifica se não é inválido e se é o digitado
 	for i in range(1, 5):
-		possiblePasswords.append(sha256(possiblePasswords[i-1].encode('utf-8')).hexdigest())
-	
-	for temp in possiblePasswords:
-		print(temp[10:16].upper())
+		possibleTokens.append(sha256(possibleTokens[i-1].encode('utf-8')).hexdigest())
+		if possibleTokens[i] in invalidatedTokens:
+			print("Token inválido!\n")
+			return
+		if token == possibleTokens[i][10:16].upper():
+			print("Autenticação realizada com sucesso!\n")
+			with open('invalidTokens', 'a') as invalidTokens:
+				invalidTokens.write("{},{}\n".format(authInfo[0], possibleTokens[i]))
+			return
+	print("Token inválido!\n")
 
 def main():
 	while True:
