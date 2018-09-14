8466e6e7b39d007416cba82ae76f4a4f0557f63c
Angelin01 <angeloborsoiross@hotmail.com>
2018-09-14 12:20:25 -0300

    Mudando para pyDes

--- T04/client.py
+++ T04/client.py
@@ -1,5 +1,5 @@
 import socket
-from Crypto.Cipher import DES  # Note: use "pycryptodome" for Python 3.6.6, not "pycrypto"
+import pyDes
 from hashlib import sha256
 from time import time
 from getpass import getpass
@@ -18,16 +18,17 @@ def main():
 		user = input("Type your username: ").strip('\n').encode('ascii')
 		# Get key, encode ascii, hash it with sha256, get the first 8 chars of the hex of the hash and encode that
 		key = sha256(getpass("Type your key: ").strip('\n').encode('ascii')).hexdigest()[:8].encode('ascii')
-		cypher = DES.new(key, DES.MODE_0FB)		
+		Kc = pyDes.des(key, pyDes.CBC, b"\0\0\0\0\0\0\0\0", pad=None, padmode=pyDes.PAD_PKCS5)		
 		resource = input("Type which resource you want to access: ").strip('\n').encode('ascii')
 
-		# Connect to AS
+		# Connect to AS on port 2222
 		sck = clientSocket(2222)
+		rn1 = str(SystemRandom().randrange(10000)).encode('ascii')
 		msgToSend = (user + b',' + 
-		             cipher.encrypt(resource + b',' + 
-		                            str(int(time())).encode('ascii') + b','
-		                            str(SystemRandom().randrange(10000))
-		                           )
+		             Kc.encrypt(resource + b',' + 
+		                        str(int(time())).encode('ascii') + b',' +
+		                        rn1
+		                       )
 		            )
 		if __debug__:
 			print("Sending to AS:\n{}".format(msgToSend))
@@ -39,12 +40,24 @@ def main():
 			print("AS server didn't answer.\nDid you input your username and key correctly?")
 			sck.close()
 			continue
-		
+		sck.close()
+
 		if __debug__:
 			print("AS answered:\n{}".format(answer))
 
-		
+		info, T_c_tgs = answer.split(b',')
+		info = Kc.decrypt(info)
 
+		# if the format is incorrect, abort
+		if info.count(b',') != 1:
+			print("Something weird happened while decrypting, aborting")
+			continue
+		K_c_tgs, n1 = info.split(b',')
+		if n1 != rn1:
+			print("Something weird happened while decrypting, aborting")
+			continue
+		#
+		#sck = clientSocket(3333)
 
 
 
