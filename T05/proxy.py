1677bfe6bf631832dc905befd50d6d42bf8d98a1
Angelin01 <angeloborsoiross@hotmail.com>
2018-11-07 17:28:20 -0200

    Proxy funcionando básico

--- T05/proxy.py
+++ T05/proxy.py
@@ -3,6 +3,43 @@ import signal
 from threading import Thread
 
 
+def answer400(conn):
+	pass
+	
+
+def answer403(conn):
+	pass
+
+
+def proxy_connect(conn, addr):
+	client_request = conn.recv(8192)
+	
+	url = client_request.split(b' ', 2)[1].split(b'/', 3)[2]
+	
+	try:
+		server_ip = socket.gethostbyname(url.decode('ISO-8859-1'))
+	except socket.gaierror:
+		pass
+		
+	print("New connection:\nClient: {}\nServer: Host: {} | IP: {}".format(addr, url.decode('ISO-8859-1'), server_ip))
+	
+	server_conn = socket.create_connection((url.decode('ISO-8859-1'), 80))
+	server_conn.settimeout(1)
+	server_conn.send(client_request)
+	while True:
+		try:
+			answer = server_conn.recv(8192)
+		except socket.timeout:
+			
+		
+		if(len(answer) > 0):
+			conn.send(answer)
+		else:
+			break
+			
+	server_conn.close()
+	conn.close()
+
 def main():
 	stop = False
 	sck = socket.socket()
@@ -12,13 +49,15 @@ def main():
 		sck.close()
 		
 	signal.signal(signal.SIGTERM, stop_handler)
-	signal.signal(signal.SIGTINT, stop_handler)
+	signal.signal(signal.SIGINT, stop_handler)
 	
 	sck.bind(('', 8000))
 	sck.listen()
-	while True:
+	
+	while not stop:
 		conn, addr = sck.accept()
-		Thread(target=proxy_connect, args=(conn,)).start()
+		Thread(target=proxy_connect, args=(conn, addr)).start()
+
 
 if __name__ == "__main__":
 	main()
