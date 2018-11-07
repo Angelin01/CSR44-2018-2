415585ac7f5178f854cbb41c84fa34011dbfc172
Angelin01 <angeloborsoiross@hotmail.com>
2018-11-07 20:02:47 -0200

    Respostas 400, 403 e 404 básicas

--- T05/proxy.py
+++ T05/proxy.py
@@ -1,59 +1,87 @@
 import socket
 import signal
 from threading import Thread
+from datetime import datetime
+
+
+def answer404(conn):
+	conn.send(("HTTP/1.1 404 Not Found\r\n" +
+	           datetime.utcnow().strftime("Date: %a, %d %b %Y %H:%M:%S") + " GMT\r\n" +
+	           "Server: AngelinProxy/1.0\r\n" +
+	           "\r\n").encode('ISO-8859-1'))
 
 
 def answer400(conn):
-	pass
-	
+	conn.send(("HTTP/1.1 400 Bad Request\r\n" +
+	           datetime.utcnow().strftime("Date: %a, %d %b %Y %H:%M:%S") + " GMT\r\n" +
+	           "Server: AngelinProxy/1.0\r\n" +
+	           "\r\n").encode('ISO-8859-1'))
+
 
 def answer403(conn):
-	pass
-
+	conn.send(("HTTP/1.1 403 Forbidden\r\n" +
+	           datetime.utcnow().strftime("Date: %a, %d %b %Y %H:%M:%S") + " GMT\r\n" +
+	           "Server: AngelinProxy/1.0\r\n" +
+	           "Content-Type: text/html\r\n" +
+	           "Content-Length: 52\r\n" +
+	           "\r\n" +
+	           "<html><body><h1>Acesso Bloqueado!</h1></body></html>").encode('ISO-8859-1'))
 
 def proxy_connect(conn, addr):
 	client_request = conn.recv(8192)
-	
+
+	if b"monitorando" in client_request:
+		answer403(conn)
+		conn.close()
+		return
+
+	if b"teste" in client_request:
+		answer404(conn)
+		conn.close()
+		return
+
 	url = client_request.split(b' ', 2)[1].split(b'/', 3)[2]
-	
+
 	try:
 		server_ip = socket.gethostbyname(url.decode('ISO-8859-1'))
 	except socket.gaierror:
 		pass
-		
+
 	print("New connection:\nClient: {}\nServer: Host: {} | IP: {}".format(addr, url.decode('ISO-8859-1'), server_ip))
-	
+
 	server_conn = socket.create_connection((url.decode('ISO-8859-1'), 80))
-	server_conn.settimeout(1)
+	#server_conn.settimeout(5)
 	server_conn.send(client_request)
 	while True:
 		try:
 			answer = server_conn.recv(8192)
 		except socket.timeout:
-			
-		
+			conn.send(answer)
+			conn.close()
+			break
+
 		if(len(answer) > 0):
 			conn.send(answer)
 		else:
 			break
-			
+
 	server_conn.close()
 	conn.close()
 
 def main():
 	stop = False
 	sck = socket.socket()
-	
+
 	def stop_handler(s, f):
 		stop = True
 		sck.close()
-		
+
 	signal.signal(signal.SIGTERM, stop_handler)
 	signal.signal(signal.SIGINT, stop_handler)
-	
+
 	sck.bind(('', 8000))
 	sck.listen()
-	
+
 	while not stop:
 		conn, addr = sck.accept()
 		Thread(target=proxy_connect, args=(conn, addr)).start()
